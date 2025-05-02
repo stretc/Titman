f8b2b3dd07b671f9e923889b617b7623986dc16f
Caleb <calebnull88@outlook.com>
2025-05-02 15:01:38 -0500

    Added GH Pages - Reinitializing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{js,jsx,ts,tsx,html}'],
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
+    screens: {
+      'sm': '430px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      'pc': '1450px',
+      '2xl': '1536px',
+    },
   },
   plugins: [],
 }
