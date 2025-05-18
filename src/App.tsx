0c65c8611e3e4f11349b47a2ff5827710177559a
Caleb <calebnull88@outlook.com>
2025-05-18 14:41:13 -0500

    Restructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
+import { useRef } from "react";
 import "./App.css";
 import { Promise } from "./components/Promise";
 import { Services } from "./components/Services";
 import { TeamMembers } from "./components/TeamMembers";
 import { TopSection } from "./components/TopSection";
 import { Footer } from "./components/Footer";
+import { BackToTop } from "./components/BackToTop";
 
 function App() {
   return (
-    <main className="overflow-x-hidden">
-      <TopSection />
-      <Services />
-      <Promise />
-      <TeamMembers />
-      <Footer />
-    </main>
+    <>
+      <BackToTop />
+      <main className="overflow-x-hidden">
+        <section id="top">
+          <TopSection />
+        </section>
+
+        <section id="services">
+          <Services />
+        </section>
+
+        <section id="promise">
+          <Promise />
+        </section>
+
+        <section id="team-members">
+          <TeamMembers />
+        </section>
+
+        <section id="footer">
+          <Footer />
+        </section>
+      </main>
+    </>
   );
 }
 
