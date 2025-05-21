c84b470fe3823279ac4301bda62e0196c4af9431
Caleb <calebnull88@outlook.com>
2025-05-21 18:32:31 -0500

    Fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx,html}'],
   theme: {
-    screens: {
-      'sm': '430px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      'pc': '1450px',
-      '2xl': '1536px',
+    extend: {
+      animation: {
+        'ping-custom': 'ping-custom 1.5s cubic-bezier(0, 0, 0.2, 1) infinite',
+      },
+      keyframes: {
+        'ping-custom': {
+          '0%': { transform: 'scale(1)', opacity: '1' },
+          '75%, 100%': { transform: 'scale(2)', opacity: '0' },
+        },
+      },
     },
   },
   plugins: [],
