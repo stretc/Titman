960a7c22d0fadfe7312dfc871159cc5746c8f1b4
Caleb <calebnull88@outlook.com>
2025-05-28 11:50:04 -0500

    changed stuff

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,4 @@
 import {
-  Clock,
-  Envelope,
   House,
   MapPin,
   Phone,
--- src/components/ui/GoogleMaps.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const GoogleMaps = () => {
-  return (
-    <div>
-      <h1>asd</h1>
-    </div>
-  );
-};
