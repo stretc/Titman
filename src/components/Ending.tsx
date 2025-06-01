173e1e80d9c8d542707779e94fbbffaf737555e8
Caleb <calebnull88@outlook.com>
2025-06-01 16:33:36 -0500

    adding finishing components

---
+++ src/components/Ending.tsx
@@ -0,0 +1,11 @@
+import "./styles/Ending.css";
+
+export const Ending = () => {
+  return (
+    <div className="ending">
+      <p>
+        <span>Thank you for visiting our website!</span>
+      </p>
+    </div>
+  );
+};
